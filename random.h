4e1519240be432b72f5a05785b4720b931c299bc
KerynCorby <tuo83192@temple.educdgit config --global user.name KerynCorby>
2024-09-03 22:39:59 -0400

    randcharHFile

---
+++ random.h
@@ -0,0 +1,8 @@
+//
+// Created by Keryn Corby on 09/03/2024.
+//
+
+#ifndef INC_3207_LAB_INTRODUCTION_F24_RANDOM_H
+#define INC_3207_LAB_INTRODUCTION_F24_RANDOM_H
+char randchar();
+#endif //INC_3207_LAB_INTRODUCTION_F24_RANDOM_H
